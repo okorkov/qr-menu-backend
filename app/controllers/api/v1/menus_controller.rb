682bf8c01b85ca5fed7e66b37d7bca86dc594555
okorkov <alex.okorkov@gmail.com>
2021-07-01 12:30:12 -0700

    calibrated qrcode specs and all link for blogs from s3

--- app/controllers/api/v1/menus_controller.rb
+++ app/controllers/api/v1/menus_controller.rb
@@ -11,9 +11,24 @@ class Api::V1::MenusController < ApplicationController
     menu = Menu.new
     menu.user = user
     menu.pdf_file.attach(menu_params['file'])
-    # menu.link = menu.pdf_file.service_url
+    menu.link = menu.pdf_file.url.sub(/\?.*/, '')
+    qrcode = RQRCode::QRCode.new(menu.link)
+    png = qrcode.as_png(
+      bit_depth: 1,
+      border_modules: 4,
+      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+      color: "black",
+      file: nil,
+      fill: "white",
+      module_px_size: 8,
+      resize_exactly_to: false,
+      resize_gte_to: false,
+      size: 400
+    )
+    menu.qr_code.attach(io: StringIO.new(png.to_s), filename: "qr_code_#{menu.id}.png")
+    menu.qr_code_link = menu.qr_code.url.sub(/\?.*/, '')
     menu.save
-    render json: {menu: menu, menu_doc: menu.pdf_file, link: menu.link}
+    render json: {menu: menu, menu_doc: menu.pdf_file, link: menu.link, qrcode: menu.qr_code_link}
   end
 
   def find_menus
