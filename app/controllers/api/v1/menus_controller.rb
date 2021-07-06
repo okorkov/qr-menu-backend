b88622332d5aa14db2c31cbedadb56b8939584b6
okorkov <alex.okorkov@gmail.com>
2021-07-06 09:55:14 -0700

    added demo action in menus controller

--- app/controllers/api/v1/menus_controller.rb
+++ app/controllers/api/v1/menus_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MenusController < ApplicationController
 
-before_action :user
+before_action :user, except: [:demo, :demo_upload]
 
   def index
     render json: {status: 'success'}
@@ -42,6 +42,35 @@ before_action :user
   def resend_qr_code
     UserMailer.send_qr_code(user).deliver_now
     render json: {status: 'success'}
+  end
+
+  def demo
+    user = User.find_by(email: 'demo@qr-menu.rest')
+    render json: {pdf_file: user.menus.first.link, qr_code: user.menus.first.qr_code_link}
+  end
+
+  def demo_upload
+    menu = Menu.new
+    menu.user = User.find_by(email: 'demo@qr-menu.rest')
+    menu.pdf_file.attach(menu_params['file'])
+    menu.link = menu.pdf_file.url.sub(/\?.*/, '')
+    qrcode = RQRCode::QRCode.new(menu.link)
+    png = qrcode.as_png(
+      bit_depth: 1,
+      border_modules: 4,
+      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+      color: "black",
+      file: nil,
+      fill: "white",
+      module_px_size: 8,
+      resize_exactly_to: false,
+      resize_gte_to: false,
+      size: 400
+    )
+    menu.qr_code.attach(io: StringIO.new(png.to_s), filename: "qr_code_#{menu.id}.png")
+    menu.qr_code_link = menu.qr_code.url.sub(/\?.*/, '')
+    menu.save
+    render json: {pdf_file: menu.link, qr_code: menu.qr_code_link}
   end
 
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
       resources :menus
       resources :users, only: [:create, :show, :index]
       post '/find_menus', to: 'menus#find_menus'
+      get '/demo', to: 'menus#demo'
       post '/resend_qr_code', to: 'menus#resend_qr_code'
+      post 'demo_upload', to: 'menus#demo_upload'
     end
   end
 
@@ -15,6 +17,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   post '/logged_in', to: 'sessions#is_logged_in?'
+
  
   
 end
