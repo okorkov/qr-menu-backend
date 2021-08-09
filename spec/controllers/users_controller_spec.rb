ae79248cfc9eb742a98f504097e14b3246900f57
okorkov <alex.okorkov@gmail.com>
2021-08-09 13:23:17 -0700

    finished tests for users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe UsersController, :type => :controller do
   before(:each) do
     @user = User.create(email: 'test@qr-menu.rest', password: 'test')
     @token = Auth.create_token(@user)
-    @file = fixture_file_upload('files/test.pdf', 'application/pdf')
   end
+
 
   it "creates user with valid credentials" do
     post :create, params: {email: 'test-register@qr-menu.rest', password: 'test', password_confirmation: 'test'}
@@ -40,11 +40,38 @@ RSpec.describe UsersController, :type => :controller do
   end
 
   it "uploads file and attaches it to the user instance" do
-    post :upload_file, params: {token: @token, file: @file}
+    file = fixture_file_upload('files/test.pdf', 'application/pdf')
+    post :upload_file, params: {token: @token, file: file}
     expect(response).to have_http_status(:ok)
     expect(response.content_type).to eq('application/json; charset=utf-8')
     expect(JSON.parse(response.body)["status"]).to eq('success')
     expect(@user.file).to be_attached
+  end
+
+  it "gets menu based off passed user id" do
+    file = fixture_file_upload('files/test.pdf', 'application/pdf')
+    @user.file.attach(file)
+    @user.file_link = @user.file.url.sub(/\?.*/, '')
+    @user.save
+    get :get_menu, params: {id: 1}
+    expect(response).to have_http_status(:ok)
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+    expect(JSON.parse(response.body)["has_file"]).to be(true)
+    expect(JSON.parse(response.body)["menu_link"]).to_not be_nil
+  end
+
+  it "returns has_file: false json if no file attached to user" do
+    get :get_menu, params: {id: 1}
+    expect(response).to have_http_status(:ok)
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+    expect(JSON.parse(response.body)["has_file"]).to be(false)
+  end
+
+  it "resends qr code to user's mail" do
+    post :resend_qr_menu, params: {token: @token}
+    expect(response).to have_http_status(:ok)
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+    expect(JSON.parse(response.body)["status"]).to eq('success')
   end
   
 
